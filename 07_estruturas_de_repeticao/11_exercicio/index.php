74d3cc04f6727f07b90eb436cffced819da3924a
DanielmGomes <31453430+DanielmGomes@users.noreply.github.com>
2021-11-12 19:07:51 -0300

    exercicios

---
+++ 07_estruturas_de_repeticao/11_exercicio/index.php
@@ -0,0 +1,12 @@
+<?php
+
+    $b = array();
+
+    for ($a = 1; $a <= 10 ; $a++) { 
+        array_push($b, $a);
+        print_r($b);
+        echo '<br>';
+        
+    }
+
+?>
---
+++ 07_estruturas_de_repeticao/12_exercicio/index.php
@@ -0,0 +1,9 @@
+<?php
+
+    $a = array();
+
+    for ($a = 10; $a <= 20 ; $a++) { 
+        echo is_float($a / 2) ? "posição: $a <br>" : "<br>";
+    }
+
+?>
